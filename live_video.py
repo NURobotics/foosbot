741a11f492adb2b3194aea5710094d382c77cd87
davi-maciel <70175298+davi-maciel@users.noreply.github.com>
2023-02-13 18:30:10 -0600

    small updates

--- live_video.py
+++ live_video.py
@@ -1,23 +1,28 @@
-from tracking import *
+from tracking_live import *
 import cv2 as cv
 
 # define a video capture object
 vid = cv.VideoCapture(0)
-  
+_frames = []
 while(True):
-      
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-        
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+  # Capture the video frame by frame
+  ret, frame = vid.read()
+  
+  # Do tracking analysis
+  frame = draw_circles(frame)
+  if len(_frames) >= 2:
+    _frames.pop(0)
+    _frames.append(frame)
+    cv.imshow('frame', _frames[-1])
+  else:
+    _frames.append(frame)
+  # Display the resulting frame
+    
+  # the 'q' button is set as the
+  # quitting button you may use any
+  # desired button of your choice
+  if cv.waitKey(1) & 0xFF == ord('q'):
+    break
   
 # After the loop release the cap object
 vid.release()
